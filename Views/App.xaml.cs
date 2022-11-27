d824ceb61c6516588ed3ef3766d00e97f25873cb
Maksym Fedorets <ltif427@gmail.com>
2022-11-28 00:25:39 +0100

    Add files

---
+++ Views/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using HSchedule.Stores;
+using HSchedule.ViewModels;
+using HSchedule.ViewModels.UserControls;
+using HSchedule.Views;
+using System.Windows;
+
+namespace HSchedule
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            NavigationStore navigationStore = new NavigationStore();
+
+            navigationStore.CurrentViewModel = new SignInViewModel(navigationStore);
+
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel(navigationStore)
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
